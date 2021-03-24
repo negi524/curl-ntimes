354ed1c9980ca63da572ba9d9eac6dfc78904395
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-03-24 22:20:52 +0900

    ファイルからURLのリストを読み込めるように修正

--- curl.sh
+++ curl.sh
@@ -2,10 +2,11 @@
 
 set -eu
 
+# URL,回数,間隔を指定してリクエストを行う
 # $1: リクエストするURL
 # $2: リクエストする回数
 # $3: リクエストする間隔
-function main() {
+function curl_ntimes() {
   local url=$1
   local times=$2
   local interval_sec=$3
@@ -20,17 +21,37 @@ function main() {
   done
 }
 
+# ファイルからURLのリストを読み込んで、リクエストを連続して行う
+# $1: 読み込みファイル名
+# $2: リクエストする間隔
+function curl_by_file() {
+  local filename=$1
+  local interval_sec=$2
+  local counter=0
+
+  echo "interval: ${interval_sec} sec"
+
+  cat ${filename} | while read url
+  do
+    counter=$((counter += 1))
+    echo "request: ${url}"
+    curl -XGET ${url} > result-${counter}.json
+    sleep ${interval_sec}
+  done
+}
+
 
 # リクエスト回数のデフォルト値
 n_times=1
-
-# リクエストする間隔のデフォルト値
+# リクエスト間隔のデフォルト値
 interval_sec=2
-
+# URLを読み込むファイル名
+url_list_file=""
+# fオプションが指定されているフラグ
+f_flag=0
 
 # オプションの処理を行う
-# TODO: ファイルを読み込んでリクエストを可能にする
-while getopts "n:i:" option
+while getopts "n:i:f:" option
 do
   case ${option} in
     n)
@@ -41,8 +62,20 @@ do
       # -iオプションでリクエスト間隔を設定できる
       interval_sec=${OPTARG}
       ;;
+    f)
+      # -fオプションでファイルから読み込める
+      url_list_file=${OPTARG}
+      # ファイルの存在チェック
+      if [ -e ${url_list_file} ]; then
+        f_flag=1
+      else
+        echo "${url_list_file}: No such file."
+        exit 1
+      fi
+      ;;
     \?)
       echo "Usage: curl.sh [-n request_tmes] [-i request_interval(sec)] target_url" 1>&2
+      echo "Usage: curl.sh [-f] file_name" 1>&2
       exit 1
       ;;
   esac
@@ -51,9 +84,13 @@ done
 # オプション指定を位置パラメータから削除する
 shift $(expr ${OPTIND} - 1)
 
-# urlを変数に格納する
-url=$1
-
-main ${url} ${n_times} ${interval_sec}
+if [ ${f_flag} -eq 1 ]; then
+  # ファイル指定でリクエストを実行する
+  curl_by_file ${url_list_file} ${interval_sec}
+else
+  # urlを変数に格納する
+  url=$1
+  curl_ntimes ${url} ${n_times} ${interval_sec}
+fi
 
 exit 0
