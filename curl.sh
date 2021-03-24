4d89de099c38b04ec78ab163d017a4e288f97747
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-03-24 21:45:08 +0900

    -iオプション追加

--- curl.sh
+++ curl.sh
@@ -2,9 +2,6 @@
 
 set -eu
 
-# リクエストする間隔
-INTERVAL_SEC=2
-
 # $1: リクエストするURL
 # $2: リクエストする回数
 # $3: リクエストする間隔
@@ -23,30 +20,40 @@ function main() {
   done
 }
 
-# オプションの処理を行う
 
 # リクエスト回数のデフォルト値
 n_times=1
 
-while getopts "n:" option
+# リクエストする間隔のデフォルト値
+interval_sec=2
+
+
+# オプションの処理を行う
+# TODO: ファイルを読み込んでリクエストを可能にする
+while getopts "n:i:" option
 do
   case ${option} in
     n)
+      # -nオプションでリクエスト回数を設定できる
       n_times=${OPTARG}
       ;;
+    i)
+      # -iオプションでリクエスト間隔を設定できる
+      interval_sec=${OPTARG}
+      ;;
     \?)
-      echo "Usage: curl.sh [-n request_tmes] target_url" 1>&2
+      echo "Usage: curl.sh [-n request_tmes] [-i request_interval(sec)] target_url" 1>&2
       exit 1
       ;;
   esac
 done
 
-# オプション位指定を一パラメータから削除する
+# オプション指定を位置パラメータから削除する
 shift $(expr ${OPTIND} - 1)
 
 # urlを変数に格納する
 url=$1
 
-main ${url} ${n_times} ${INTERVAL_SEC}
+main ${url} ${n_times} ${interval_sec}
 
 exit 0
